d1ab24b6dd8e186291a2efe55fc5480a4a80e9b3
Holly Ojeda <Razzinator@live.com>
2020-01-02 12:29:07 -0500

    Added fuzzy matching spike. Beginning of validators and cleaners with minimum required function.

---
+++ source/validators.py
@@ -0,0 +1,90 @@
+"""
+	Filename: validators.py
+	Programmer: Holly Locke
+	Date Created: 01/02/2020
+	Last Update: File Created
+
+	Description: This file contains all validation logic that may be applied to the columns. 
+	Built to work with the rest of the data tool, validators are created as classes.
+
+"""
+class NUMERIC_ID:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class MIXED_ID:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class PLAIN_TEXT:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class MIXED_TEXT:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class SECTION_CODE:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class GRADE_ITEM_CATEGORY:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class GRADE_ITEM_NAME:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class GRADE_VALUE:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
+
+class DATE:
+	messages = list()
+	errors = list()
+
+	def __init__(self, column)
+		self.column = column
+
+	def run():
---
+++ spikes/Fuzzy Matching/matcher.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from pprint import pprint
+
+import editdistance
+
+threshold = 3
+
+def summary(data):
+	#Get lexicon
+	uniqueWords = set()
+	for word in data: uniqueWords.add(word)
+	
+	print("Unique Entries: [%i]" % (data.nunique()))
+	print("Most Repeated Entries:")
+	print(data.value_counts())
+	
+	
+	
+"""Parser will read from a PD column, manage sending the right data type"""
+def read_column():
+	data = pd.read_csv("testfile.txt")
+	
+	return(data["Grade Item Name"])
+	
+"""Read in a pandas column and perform fuzzy matching."""
+def clean(pdColumn):
+	#Get lexicon
+	uniqueWords = set()
+	for word in data: uniqueWords.add(word)
+	
+	#Nearest Distance
+	minimumEdit = 20
+	viableEdits = list()
+	editList = dict()
+	
+	#Iterate over the lexicon, finding words that fit within the edit distance threshold
+	for item in pdColumn:
+		for word in uniqueWords:
+			distance = editdistance.eval(item, word)
+			if word != item and distance < threshold:
+				viableEdits.append(word)
+				
+			print("Matching [%s] to [%s]: Edit distance %s" % (item, word, distance))
+				
+		editList[item] = viableEdits
+		viableEdits = list()
+	pprint(editList)
+	
+	#Print summary of uncleaned data
+	summary(pdColumn)
+	
+
+if __name__ == "__main__":
+	data = read_column()
+	
+	clean(data)
