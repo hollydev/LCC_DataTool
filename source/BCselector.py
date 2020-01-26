95bda0852b2af14dbb8b311bbe56d8a74144a33e
S34NT <thompsonsean13@gmail.com>
2020-01-26 00:14:54 -0500

    Changed to select by name, CourseOffering code split and replaced with new columns

--- source/BCselector.py
+++ source/BCselector.py
@@ -8,122 +8,256 @@
 
 import pandas as pd 
 import glob as gb 
-from enum import Enum 
-
-from messages.validators import validators
+import validators
 #from cleaners import GRADE_ITEM_NAME
 
-class BASE_COLUMN(Enum):
-    userName = 1
-    firstName = 2
-    lastName = 3
-    roleID = 4
-    roleName = 5
-    courseOfferingID = 6
-    courseOfferingCode = 7
-    courseOfferingName = 8
-    CourseSectionCode = 9
-    gradeItemCategoryID = 10
-    gradeItemCategoryName = 11
-    gradeItemID = 12
-    gradeItemName = 13
-    gradeItemWeight = 14
-    pointsNumerator = 15
-    pointsdenominator = 16
-    gradeValue = 17
-    gradeLastModified = 18
-     
-
 #setting for the displayed output (For testing)
-pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', 8)
 pd.set_option('display.width', None)
-#pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', -1)
+
 
 #this method accepts a string that is then used to compare against the enum values
 def get_base_column(theColumn):
+
+
     #using glob to find all files in the subdirectory 'GBinfo' that have the .csv extension
-    
     for file in gb.glob("./GBinfo/*.csv"):
 
-        #call method for rearranging the columns..?
+        #grab the base columns from the csv file
         df = pd.read_csv(file, usecols = list(range(0,18)), sep = ',')
 
-        tempdf = df[['name', 'number', 'term']] = df.CourseOfferingCode.str.split("-", expand = True)
-
-        pd.concat([df, tempdf], axis=1)
-
-        print(df)
-
-
+        #send the dataframe to split_and_reorganize so it can be modified
+        df = split_and_reorganize(df)
+        #convert column headers to lowercase
+        df.columns = [x.lower() for x in df.columns]
         
-        for column in BASE_COLUMN:
-            if(theColumn == column.name):
-                x = column.value - 1  
-                
-                                              #Target column
-        df = pd.read_csv(file, usecols = list(range(x, x+1)), sep = ',')
-        
-        if(theColumn == "userName"):
-            print(df)
+        #Target column
+        df = df.loc[:,theColumn.lower()]
 
-        elif(theColumn == "firstName"):
-            print(df)
 
-        elif(theColumn == "lastName"):
-            print(df)
-
-        elif(theColumn == "roleID"):
-            print(df)
-
-        elif(theColumn == "courseOfferingID"):
-            print(df)
-
-        elif(theColumn == "courseOfferingCode"):
-            print(df)
-
-        elif(theColumn == "courseOfferingName"):
-            print(df)
-
-        elif(theColumn == "courseSectionCode"):
-            print(df)
-        
-        elif(theColumn == "gradeItemCategoryID"):
-            print(df)
-
-        elif(theColumn == "gradeItemCategoryName"):
-            validateMixedID = validators.MIXED_ID(df)
+        #Determine the name of the column and call appropriate validators
+        if(theColumn.lower() == "username"):
+            validateMixedID = validators.MIXED_TEXT(df)
             validateMixedID.run()
             info = validateMixedID.statistics()
-            print(info)
-            #print(df)
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
 
-        elif(theColumn == "gradeItemID"):
+            print(info)
+            print(warnings)
+            print(errors)
+            
+
+        elif(theColumn.lower() == "firstname"):
+            validatePlainText = validators.PLAIN_TEXT(df)
+            validatePlainText.run()
+            info = validatePlainText.statistics()
+            warnings = validatePlainText.get_warnings()
+            errors = validatePlainText.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+            
+
+        elif(theColumn.lower() == "lastname"):
+            validatePlainText = validators.PLAIN_TEXT(df)
+            validatePlainText.run()
+            info = validatePlainText.statistics()
+            warnings = validatePlainText.get_warnings()
+            errors = validatePlainText.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+            
+
+        elif(theColumn.lower() == "roleid"):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(3)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == "rolename"):
+            validatePlainText = validators.PLAIN_TEXT(df)
+            validatePlainText.run()
+            info = validatePlainText.statistics()
+            warnings = validatePlainText.get_warnings()
+            errors = validatePlainText.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == "courseofferingid"):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(6)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+            
+
+        elif(theColumn.lower() == "name"):
+            validateMixedID = validators.MIXED_TEXT(df)
+            validateMixedID.run()
+            info = validateMixedID.statistics()
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == 'number'):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(5)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == 'term'):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(6)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == "courseofferingname"):
+            validateMixedID = validators.MIXED_TEXT(df)
+            validateMixedID.run()
+            info = validateMixedID.statistics()
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == "coursesectioncode"):
+            validateMixedID = validators.MIXED_TEXT(df)
+            validateMixedID.run()
+            info = validateMixedID.statistics()
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+        
+        elif(theColumn.lower() == "gradeitemcategoryid"):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(6)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+
+        elif(theColumn.lower() == "gradeitemcategoryname"):
+            validateMixedID = validators.MIXED_TEXT(df)
+            validateMixedID.run()
+            info = validateMixedID.statistics()
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+            
+        elif(theColumn.lower() == "gradeitemid"):
             validateNumeric = validators.NUMERIC_ID(df)
             validateNumeric.run(7)
             info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
             print(info)
+            print(warnings)
+            print(errors)
         
-        elif(theColumn == "gradeItemName"):
-            print(df)
+        elif(theColumn.lower() == "gradeitemname"):
+            validateMixedID = validators.MIXED_TEXT(df)
+            validateMixedID.run()
+            info = validateMixedID.statistics()
+            warnings = validateMixedID.get_warnings()
+            errors = validateMixedID.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+            
         
-        elif(theColumn == "gradeItemWeight"):
+        elif(theColumn.lower() == "gradeitemweight"):
             print(df)
 
-        elif(theColumn == "pointsNumerator"):
+        elif(theColumn.lower() == "pointsnumerator"):
             print(df)
         
-        elif(theColumn == "pointsDenominator"):
+        elif(theColumn.lower() == "pointsdenominator"):
+            validateNumeric = validators.NUMERIC_ID(df)
+            validateNumeric.run(7)
+            info = validateNumeric.statistics()
+            warnings = validateNumeric.get_warnings()
+            errors = validateNumeric.get_errors()
+
+            print(info)
+            print(warnings)
+            print(errors)
+        
+        elif(theColumn.lower() == "gradevalue"):
             print(df)
         
-        elif(theColumn == "gradeValue"):
-            print(df)
-        
-        elif(theColumn == "gradeLastModified"):
+        elif(theColumn.lower() == "gradelastmodified"):
             print(df)
                 
     return df
-        
+
+
+#splits CourseOfferingCode into three columns and replaces it with the new columns
+def split_and_reorganize(theDataFrame):
+
+    #df1 takes all the columns up to CourseOfferingCode
+    df1 = pd.DataFrame(theDataFrame.iloc[:, :7])
+
+    #CourseOfferingCode is split, new columns are appended and CourseOfferingCode is dropped
+    df1[['name', 'number', 'term']] = theDataFrame.CourseOfferingCode.str.split("-", expand = True)
+    df1 = df1.drop(['CourseOfferingCode'], axis = 1)
+
+    #df2 takes all the columns after CourseOfferingCode
+    df2 = pd.DataFrame(theDataFrame.iloc[:, 7:])
+
+    #df1 and df2 are concatenated and returned
+    frames = [df1, df2]
+    theDataFrame = pd.concat(frames, sort = False, axis = 1)
+    return theDataFrame
+
+
 
 if __name__ == '__main__':
-    get_base_column("gradeItemCategoryName")
-    get_base_column("gradeItemID")
+    
+    get_base_column("courseOfferingName")
+
+    get_base_column("roleName")
+
+   
+
