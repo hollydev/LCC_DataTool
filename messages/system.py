14e581b34de9d1f36c805bf30836a1fccc731100
S34NT <thompsonsean13@gmail.com>
2020-01-26 14:59:30 -0500

    added the warning messages for validators and cleaners

--- messages/system.py
+++ messages/system.py
@@ -10,6 +10,44 @@
 """
 
 ################
+## Validators ##
+################
+class VALIDATORS:
+	def __init__(self):
+		#Numeric ID
+		self.success = "Validator [%s] ran successfully."
+		self.failure = "Validator [%s] failed to pass."
+		self.notNumeric = "%s contains a non-numeric character."
+		self.notUniqueDup = "%i ID is not unique. Duplicate entry found at %i"
+		self.notUniqueMult = "Multiple IDs found. Duplicate entries found at %s"
+		self.length = "%i is of length %i, expected length %i"
+
+
+		#Mixed ID
+		self.expectMixedNum = "%s contains only numeric values, expected mixed values"
+		self.expectMixedChar = "%s contains only character values, expected mixed values"
+		self.expectedNonEmpty = "%s contains no values"
+
+
+		#PlainText
+		self.expectedTextOnly = "%s contains a digit"
+		self.expectedNoSymbols = "%s contains a symbol"
+
+
+
+################
+##  Cleaners  ##
+################
+class CLEANERS:
+
+	def __init__(self):
+		#Cleaners - Numeric ID
+		self.changedNumeric = "Removed non-numeric character in %s. Changed to %i"
+
+
+
+
+################
 ##   System   ##
 ################
 class SYSTEM:
