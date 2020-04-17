77780d99ced3f4c9a975b43bfa0d8bd7cae2d5ad
Travis-Siebenmark <tsiebenmark@gmail.com>
2020-04-17 12:18:08 -0400

    added system unittest and mock getFiles

---
+++ src/unittest/python/test_system/test_system.py
@@ -0,0 +1,19 @@
+import unittest
+import src.main.python.lcc_assessment.system as system
+
+class test_system(unittest.TestCase):
+    def test_get_last(self):
+        name = "Joe Smith"
+        self.assertEqual(system.get_last(name), "Smith")
+        name = "John Doe"
+        self.assertEqual(system.get_last(name), "Doe")
+        name = "Jim"
+        self.assertEqual(system.get_last(name), "Jim")
+        name = "Bob John Joe"
+        self.assertEqual(system.get_last(name), "Joe")
+        name = "This is a test"
+        self.assertEqual(system.get_last(name), "test")
+        
+    
+if __name__  == "__main__":
+    unittest.main()
