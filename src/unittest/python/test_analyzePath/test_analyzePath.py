3b1622a33e33bc8640db06238f62102b3405b6ca
Travis-Siebenmark <tsiebenmark@gmail.com>
2020-04-16 22:27:58 -0400

    Fixed analyzePath tests

--- src/unittest/python/test_analyzePath/test_analyzePath.py
+++ src/unittest/python/test_analyzePath/test_analyzePath.py
@@ -1,24 +1,31 @@
 import unittest
-import analyzePath
-import getFiles
+
+import src.main.python.lcc_assessment.analyzePath as analyzePath
 
 class Test_analyzeData(unittest.TestCase):
     
-    files = getFiles.get_files("./dirpath")
-    
     def test_ProgramArea(self):
-        self.assertEqual(analyzePath.get_program_area(self.files[0]),'A&S')
-        self.assertEqual(analyzePath.get_program_area(self.files[20]),'A&S')
+        path = r"D2L Data\A&S\AAST\Spring 19\fileName"
+        self.assertEqual(analyzePath.get_program_area(path),'A&S')
+        path = r"D2L Data\A&S\English-SocialScience\Archived\Fall 15\fileName"
+        self.assertEqual(analyzePath.get_program_area(path),'A&S')
         self.assertEqual(analyzePath.get_program_area(""),'N/A')
-        self.assertEqual(analyzePath.get_program_area(self.files[11000]),'HHS')
-        self.assertEqual(analyzePath.get_program_area(self.files[15000]),'TC')
+        path = "D2L Data\HHS\Spring 2019\fileName"
+        self.assertEqual(analyzePath.get_program_area(path),'HHS')
+        path = r"D2L Data\TC\Archived\Spring 16\fileName"
+        self.assertEqual(analyzePath.get_program_area(path),'TC')
         
     def test_Department(self):
-        self.assertEqual(analyzePath.get_department(self.files[0]),'N/A')
-        self.assertEqual(analyzePath.get_department(self.files[20]),'AAST')
-        self.assertEqual(analyzePath.get_department(self.files[8000]),'Science-Math')
-        self.assertEqual(analyzePath.get_department(self.files[9999]),'Science-Math')
-        self.assertEqual(analyzePath.get_department(self.files[15000]),'N/A')
+        path = r"D2L Data\A&S\English-SocialScience\Archived\Summer 17\fileName"
+        self.assertEqual(analyzePath.get_department(path),'English-SocialScience')
+        path = r"D2L Data\A&S\AAST\Spring 19\fileName"
+        self.assertEqual(analyzePath.get_department(path),'AAST')
+        path = r"D2L Data\A&S\Science-Math\Archived\Fall 16\fileName"
+        self.assertEqual(analyzePath.get_department(path),'Science-Math')
+        path = r"D2L Data\A&S\Science-Math\Fall 19\fileName"
+        self.assertEqual(analyzePath.get_department(path),'Science-Math')
+        path = r"D2L Data\TC\Archived\Fall 15\fileName"
+        self.assertEqual(analyzePath.get_department(path),'N/A')
         
 if __name__ == '__main__':
     unittest.main()
